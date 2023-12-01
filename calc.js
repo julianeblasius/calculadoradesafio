723adde663dc387518231f8003930bd67dcb310a
Jaotarzan <joaovssouza59@gmail.com>
2023-12-01 17:01:25 -0300

    falta so algumas

--- calc.js
+++ calc.js
@@ -8,3 +8,13 @@ limpar.addEventListener("click", function() {
     resultado = ""
     document.getElementById("resultado").innerHTML = resultado;
 })
+
+let igual = document.querySelector("#igual");
+igual.addEventListener("click", function() {
+    let resultado = document.getElementById("resultado").innerHTML;
+    resultado = resultado.replace("^", "**").replace("%", "/100");
+    console.log(resultado)
+    resultado = eval(resultado);
+    document.getElementById("resultado").innerHTML = resultado;
+})
+
