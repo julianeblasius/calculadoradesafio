a4f4a71a09a923268c817eca6cae734fda258451
Jaotarzan <joaovssouza59@gmail.com>
2023-12-04 22:08:54 -0300

    aaaaaa

--- calc.js
+++ calc.js
@@ -3,6 +3,9 @@ function digitar(a) {
     document.getElementById("resultado").innerHTML = resultado;
 }
 
+let numeros = document.querySelectorAll(".botao");
+numeros.addEventListener("keypress", digitar)
+
 let limpar = document.querySelector("#limpa");
 limpar.addEventListener("click", function() {
     resultado = ""
@@ -12,7 +15,7 @@ limpar.addEventListener("click", function() {
 let igual = document.querySelector("#igual");
 igual.addEventListener("click", function() {
     let resultado = document.getElementById("resultado").innerHTML;
-    resultado = resultado.replace("^", "**").replace("%", "/100");
+    resultado = resultado.replace("^", "**").replace("%", "/100").replace("sin", "Math.sin");
     console.log(resultado)
     resultado = eval(resultado);
     document.getElementById("resultado").innerHTML = resultado;
